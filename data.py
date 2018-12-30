5e0ccd98e60c760f5b4b67bc5a4264a1480ba2a5
rohitkrroy7 <rohitkrroy@outlook.com>
2018-12-31 01:22:29 +0530

    initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'This is my first article to the myFlaskApp! It feels really cool to work on this framework.',
+            'author':'Rohit Roy',
+            'create_date':'12-29-2018'
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'This is my Second article to the myFlaskApp! It feels really cool to work on this framework.',
+            'author':'Rohit Roy',
+            'create_date':'12-30-2018'
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'This is my Third article to the myFlaskApp! It feels really cool to work on this framework.',
+            'author':'Rohit Roy',
+            'create_date':'12-31-2018'
+        }
+    ]
+    return articles
